b5a09c789036ff5af681cd154c8a52439054676d
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-09-05 22:15:21 +0300

    add prettier format

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,42 +2,31 @@ import throttle from 'lodash.throttle';
 import Player from '@vimeo/player/dist/player.js';
 // console.log(Player.prototype);
 
-
 const iframe = document.querySelector('iframe');
 // console.log(iframe);
 
 const player = new Player(iframe);
 // console.log(player);
 
-
-const STORAGE_TIME = "videoplayer-current-time";
+const STORAGE_TIME = 'videoplayer-current-time';
 
 function onPlayTimeSaver(event) {
-    let playTime = event.seconds;
-    localStorage.setItem(STORAGE_TIME, playTime);
-// console.log("значение времени", playTime);
-    
-};
+  let playTime = event.seconds;
+  localStorage.setItem(STORAGE_TIME, playTime);
+  // console.log("значение времени", playTime);
+}
 
 player.on('timeupdate', throttle(onPlayTimeSaver, 1000));
 
 let lastTimeValue = localStorage.getItem(STORAGE_TIME);
 // console.log(lastTimeValue);
 if (lastTimeValue) {
-player.setCurrentTime(lastTimeValue)};
-
-
-
+  player.setCurrentTime(lastTimeValue);
+}
 
 // event listener
 
-// const logPlayerPlay = function(event) { 
+// const logPlayerPlay = function(event) {
 //     console.log("плеер играет")};
 
 // player.on('play', logPlayerPlay);
-
-
-
-
-
-
