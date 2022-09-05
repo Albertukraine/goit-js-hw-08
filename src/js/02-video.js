db20c4a6ef29dc098e27550efe36152f3acdc8a9
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-09-05 21:47:46 +0300

    resolve task2 task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player/dist/player.js';
+// console.log(Player.prototype);
+
+
+const iframe = document.querySelector('iframe');
+// console.log(iframe);
+
+const player = new Player(iframe);
+// console.log(player);
+
+
+const STORAGE_TIME = "videoplayer-current-time";
+
+function onPlayTimeSaver(event) {
+    let playTime = event.seconds;
+    localStorage.setItem(STORAGE_TIME, playTime);
+// console.log("значение времени", playTime);
+    
+};
+
+player.on('timeupdate', throttle(onPlayTimeSaver, 1000));
+
+let lastTimeValue = localStorage.getItem(STORAGE_TIME);
+// console.log(lastTimeValue);
+if (lastTimeValue) {
+player.setCurrentTime(lastTimeValue)};
+
+
+
+
+// event listener
+
+// const logPlayerPlay = function(event) { 
+//     console.log("плеер играет")};
+
+// player.on('play', logPlayerPlay);
+
+
+
+
+
 
