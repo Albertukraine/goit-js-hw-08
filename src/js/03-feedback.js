2079b774bc25056aab2314d641fb84e14e130818
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-09-09 23:05:29 +0300

    resolve remarks from menthor

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,66 +3,63 @@ import throttle from 'lodash.throttle';
 const refs = {
   emailEl: document.querySelector('input'),
   textAreaEl: document.querySelector('textarea'),
-  submitButtonEl: document.querySelector('form'),
+  formEl: document.querySelector('.feedback-form'),
 };
+
+// refs.textAreaEl.addEventListener('input', throttle(onTextareaInput, 500));
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.formEl.addEventListener('input', throttle(onInput, 500));
 
 const USER_DATA = 'feedback-form-state';
 
-refs.emailEl.addEventListener('input', throttle(onEmailInput, 500));
-refs.textAreaEl.addEventListener('input', throttle(onTextareaInput, 500));
-refs.submitButtonEl.addEventListener('submit', onFormSubmit);
+const storageObj = {};
 
-let emailValue = '';
-let textareaValue = '';
-
-const storageObj = {
-  email: emailValue,
-  message: textareaValue,
-};
-
+// Сохраняет в локальное хранилище браузера из обьекта кода, вызывается во время инпута в поля формы
 function savingObjectToLocalStorage() {
   const userDataJSON = JSON.stringify(storageObj);
   localStorage.setItem(USER_DATA, userDataJSON);
 }
 
 // function setFeedback() {localStorage.setItem(USER_COMMENT, textareaValue);}
-
-function onEmailInput(event) {
-  emailValue = event.target.value;
-  storageObj.email = emailValue;
-  // console.log(emailValue);
+// записывыет в обьект результат события при вводе  в поле почта
+function onInput(event) {
+  dataToObject();
+  event.target.type === 'email'
+    ? (storageObj.email = event.target.value)
+    : (storageObj.message = event.target.value);
   savingObjectToLocalStorage();
 }
 
-function onTextareaInput(event) {
-  textareaValue = event.target.value;
-  storageObj.message = textareaValue;
-  savingObjectToLocalStorage();
-  // setFeedback();
+//  записывает в обьект при вводе текста в поле текст
 
-  // console.log(textareaValue)
-}
-
-function onFormSubmit(event) {
-  console.log('email:', emailValue, 'message:', textareaValue);
-  event.preventDefault();
-  event.currentTarget.reset();
+function clearStorageAndFields() {
+  refs.formEl.reset();
   localStorage.removeItem(USER_DATA);
 }
 
-function getDataFromStorage() {
-  const savedUserDataString = localStorage.getItem(USER_DATA);
-  const savedUserDataObj = JSON.parse(savedUserDataString);
+function fillFormFields() {
+  const savedUserDataObj = JSON.parse(localStorage.getItem(USER_DATA));
   if (savedUserDataObj) {
     refs.textAreaEl.value = savedUserDataObj.message;
     refs.emailEl.value = savedUserDataObj.email;
   }
 }
 
-getDataFromStorage();
-// console.log(storageObj);
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (refs.emailEl.value === '' || refs.textAreaEl.value === '') {
+    return alert('Please check that both fields are filled');
+  }
+  dataToObject();
+  console.log(storageObj);
+  clearStorageAndFields();
+}
 
-// function onInputEmailListener() {emailEl.addEventListener("input", console.log(emailEl.value))};
-// console.log(onInputEmailListener);
+// запись данных из локального хранилища как значение обьекта кода
+function dataToObject() {
+  storageObj.email = refs.emailEl.value;
+  storageObj.message = refs.textAreaEl.value;
+}
+fillFormFields();
 
-// emailEl.addEventListener("input", console.log(emailEl))
+
